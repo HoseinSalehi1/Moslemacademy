a228522cac53402ccf9ed887ecbbc3445622efaf
Hosein Ali Salehi <103531326+HoseinSalehi1@users.noreply.github.com>
2022-12-31 00:36:18 +0430

    Add files via upload

---
+++ moslim academy/Quiz/js/questions.js
@@ -0,0 +1,95 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "پایتخت افغانستان کدام شهر است؟",
+    answer: "کابل",
+    options: [
+      "هرات",
+      "کندهار",
+      "کابل",
+      "بلخ"
+    ]
+  },
+    {
+    numb: 2,
+    question: "   آرامگاه احمدشاه ابدالی در کدام شهر قرار دارد؟",
+    answer: "کندهار",
+    options: [
+      "هرات",
+      "کابل",
+      "نیمروز",
+      "کندهار"
+    ]
+  },
+    {
+    numb: 3,
+    question: " مرکز ولایت نیمروز کدام شهر است؟",
+    answer: "زرنج",
+    options: [
+      "خاشرود",
+      "زرنج",
+      "کنگ",
+      "هیچکدام "
+    ]
+  },
+    {
+    numb: 4,
+    question: "آکادمی مسلم در کدام سال افتتاح شد؟",
+    answer: "2015",
+    options: [
+      "2017",
+      "2010",
+      "2015",
+      "2016"
+    ]
+  },
+    {
+    numb: 5,
+    question: "مدیر آکادمی مسلم کیست؟",
+    answer: "استاد محمدشعیب حیات",
+    options: [
+      "استاد عبدالهادی بیدار",
+      "استاد شریف الله شریفی",
+      "استاد محمدشعیب حیات",
+      "استاد عبدالغفار محمدی"
+    ]
+    
+  },
+  {
+    numb: 5,
+    question: " ام ابلاد لقب کدام شهر است؟",
+    answer: "بلخ",
+    options: [
+      " غزنی",
+      "بلخ",
+      "غور",
+      "هرات"
+    ]
+  },
+  {
+    numb: 4,
+    question: "معنی کلمه sesame چیست؟",
+    answer: "کنجد",
+    options: [
+      "زیتون",
+      "کنجد",
+      "شوید",
+      "فندق"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ moslim academy/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "پایتخت ایران کدام شهر است؟",
+    answer: "تهران",
+    options: [
+      "شیراز",
+      "اصفهان",
+      "تهران",
+      "مشهد"
+    ]
+  },
+    {
+    numb: 2,
+    question: "سی و سه پل در کجا قرار دارد؟",
+    answer: "اصفهان",
+    options: [
+      "ایلام",
+      "اصفهان",
+      "خوزستان",
+      "شیراز"
+    ]
+  },
+    {
+    numb: 3,
+    question: "مخترع برق کیست ؟",
+    answer: "تسلا",
+    options: [
+      "ادیسون",
+      "نیوتون",
+      "تسلا",
+      "بیل گیتس"
+    ]
+  },
+    {
+    numb: 4,
+    question: "آرامگاه حافظ در کجا قرار دارد؟",
+    answer: "شیراز",
+    options: [
+      "تهران",
+      "مشهد",
+      "اصفهان",
+      "شیراز"
+    ]
+  },
+    {
+    numb: 5,
+    question: "پایتخت مذهبی ایران کجاست؟",
+    answer: "مشهد",
+    options: [
+      "قم",
+      "مشهد",
+      "تهران",
+      "همدان"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
